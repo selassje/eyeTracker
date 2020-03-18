e2c98d0488a69f76a8acf81f4983667c072d52cf
Przemyslaw Koziol <przemkoz85@gmail.com>
2020-03-18 17:06:47 +0100

    --cmake changes

--- src/EyeTracker.cpp
+++ src/EyeTracker.cpp
@@ -13,7 +13,7 @@
 // CEyeTrackerApp
 
 BEGIN_MESSAGE_MAP(CEyeTrackerApp, CWinApp)
-	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+    ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
 END_MESSAGE_MAP()
 
 
@@ -21,8 +21,8 @@ END_MESSAGE_MAP()
 
 CEyeTrackerApp::CEyeTrackerApp()
 {
-	// TODO: add construction code here,
-	// Place all significant initialization in InitInstance
+    // TODO: add construction code here,
+    // Place all significant initialization in InitInstance
 }
 
 
@@ -35,130 +35,129 @@ CEyeTrackerApp theApp;
 
 BOOL CEyeTrackerApp::InitInstance()
 {
-	// InitCommonControlsEx() is required on Windows XP if an application
-	// manifest specifies use of ComCtl32.dll version 6 or later to enable
-	// visual styles.  Otherwise, any window creation will fail.
-	INITCOMMONCONTROLSEX InitCtrls;
-	InitCtrls.dwSize = sizeof(InitCtrls);
-	// Set this to include all the common control classes you want to use
-	// in your application.
-	InitCtrls.dwICC = ICC_WIN95_CLASSES;
-	InitCommonControlsEx(&InitCtrls);
+    // InitCommonControlsEx() is required on Windows XP if an application
+    // manifest specifies use of ComCtl32.dll version 6 or later to enable
+    // visual styles.  Otherwise, any window creation will fail.
+    INITCOMMONCONTROLSEX InitCtrls;
+    InitCtrls.dwSize = sizeof(InitCtrls);
+    // Set this to include all the common control classes you want to use
+    // in your application.
+    InitCtrls.dwICC = ICC_WIN95_CLASSES;
+    InitCommonControlsEx(&InitCtrls);
 
-	CWinApp::InitInstance();
+    CWinApp::InitInstance();
 
-	AfxEnableControlContainer();
+    AfxEnableControlContainer();
 
-	// Standard initialization
-	// If you are not using these features and wish to reduce the size
-	// of your final executable, you should remove from the following
-	// the specific initialization routines you do not need
-	// Change the registry key under which our settings are stored
-	// TODO: You should modify this string to be something appropriate
-	// such as the name of your company or organization
-	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+    // Standard initialization
+    // If you are not using these features and wish to reduce the size
+    // of your final executable, you should remove from the following
+    // the specific initialization routines you do not need
+    // Change the registry key under which our settings are stored
+    // TODO: You should modify this string to be something appropriate
+    // such as the name of your company or organization
+    SetRegistryKey(_T("Local AppWizard-Generated Applications"));
 
-	CEyeTrackerDlg dlg;
-	m_pMainWnd = &dlg;
-    ::MessageBox(m_pMainWnd->m_hWnd, L"Hi.", L"Error", MB_ICONERROR | MB_OK);
-	typedef HRESULT (STDAPICALLTYPE* FuncDllRegisterServer)();
-	
+    CEyeTrackerDlg dlg;
+    m_pMainWnd = &dlg;
+#ifdef DEBUG
+    typedef HRESULT(STDAPICALLTYPE* FuncDllRegisterServer)();
+
 #ifndef _AFXDLL
-	HMODULE hDll = LoadLibrary(L"NTGraph.ocx");
+    HMODULE hDll = LoadLibrary(L"NTGraph.ocx");
 #else
-	HMODULE hDll = AfxLoadLibrary(L"NTGraph.ocx");
+    HMODULE hDll = AfxLoadLibrary(L"NTGraph.ocx");
 #endif	
-	if (hDll==0)
-	{
-		//::MessageBox(m_pMainWnd->m_hWnd,L"Could not find NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
-	}
-	else
-	{
-		FuncDllRegisterServer registerServer = (FuncDllRegisterServer)GetProcAddress(hDll, "DllRegisterServer");	
-		if (registerServer == 0)
-		{
-			
-			exit(-1);
-		}
-		else
-		{
-			HRESULT hResult;
-			hResult = registerServer();
-			if(FAILED(hResult))
-			{
-				::MessageBox(m_pMainWnd->m_hWnd,L"Could not register NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
-				//exit(-1);
-			}
-		}	
-	
-	
-	
-	
-	
-	
-	}
+    if (hDll == 0)
+    {
+        ::MessageBox(m_pMainWnd->m_hWnd,L"Could not find NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
+    }
+    else
+    {
+        FuncDllRegisterServer registerServer = (FuncDllRegisterServer)GetProcAddress(hDll, "DllRegisterServer");
+        if (registerServer == 0)
+        {
 
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
+            exit(-1);
+        }
+        else
+        {
+            HRESULT hResult;
+            hResult = registerServer();
+            if (FAILED(hResult))
+            {
+                ::MessageBox(m_pMainWnd->m_hWnd, L"Could not register NTGraph.ocx.", L"Error", MB_ICONERROR | MB_OK);
+                //exit(-1);
+            }
+        }
 
-	// Since the dialog has been closed, return FALSE so that we exit the
-	//  application, rather than start the application's message pump.
-    
 
-	if (hDll)
-	{
-		FuncDllRegisterServer unregisterServer = (FuncDllRegisterServer)GetProcAddress(hDll, "DllUnregisterServer");	
-		if (unregisterServer == 0)
-		{
-			//::MessageBox(m_pMainWnd->m_hWnd,L"Could not find NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
-		}
-		else
-		{
-		  HRESULT hResult =	unregisterServer();
-		  if(FAILED(hResult))
-		  {
-				//::MessageBox(m_pMainWnd->m_hWnd,L"Could not unregister NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
-		  }	
-		}	
-	}
-	
-    
-	 return FALSE;
-     
+
+
+
+
+    }
+#endif DEBUG
+    INT_PTR nResponse = dlg.DoModal();
+    if (nResponse == IDOK)
+    {
+        // TODO: Place code here to handle when the dialog is
+        //  dismissed with OK
+    }
+    else if (nResponse == IDCANCEL)
+    {
+        // TODO: Place code here to handle when the dialog is
+        //  dismissed with Cancel
+    }
+
+    // Since the dialog has been closed, return FALSE so that we exit the
+    //  application, rather than start the application's message pump.
+
+#ifdef DEBUG
+    if (hDll)
+    {
+        FuncDllRegisterServer unregisterServer = (FuncDllRegisterServer)GetProcAddress(hDll, "DllUnregisterServer");
+        if (unregisterServer == 0)
+        {
+            //::MessageBox(m_pMainWnd->m_hWnd,L"Could not find NTGraph.ocx.", L"Error",MB_ICONERROR | MB_OK);
+        }
+        else
+        {
+            HRESULT hResult = unregisterServer();
+            if (FAILED(hResult))
+            {
+                ::MessageBox(m_pMainWnd->m_hWnd, L"Could not unregister NTGraph.ocx.", L"Error", MB_ICONERROR | MB_OK);
+            }
+        }
+    }
+
+#endif
+    return FALSE;
 }
 
 
 extern CEyeTrackerDlg* EyeTrackerDlg;
 
 void Log(CString strMessage)
-{	
-	if(EyeTrackerDlg)
-		EyeTrackerDlg->Log(strMessage);
+{
+    if (EyeTrackerDlg)
+        EyeTrackerDlg->Log(strMessage);
 }
 
-void SetDlgItemDouble(CWnd* pWnd,int iIdc,double dValue)
+void SetDlgItemDouble(CWnd* pWnd, int iIdc, double dValue)
 {
-	CString strString;
-	strString.Format(L"%.2f",dValue);
-	SetDlgItemText(pWnd->m_hWnd,iIdc,strString);
+    CString strString;
+    strString.Format(L"%.2f", dValue);
+    SetDlgItemText(pWnd->m_hWnd, iIdc, strString);
 }
 
-double GetDlgItemDouble(CWnd* pWnd,int iIdc)
+double GetDlgItemDouble(CWnd* pWnd, int iIdc)
 {
-	
-	CString strString;
-	GetDlgItemText(pWnd->m_hWnd,iIdc,(LPWSTR) strString.GetBuffer(100),100);
-	double dValue = wcstod(strString,NULL);
-	strString.ReleaseBuffer();
 
-	return dValue;
+    CString strString;
+    GetDlgItemText(pWnd->m_hWnd, iIdc, (LPWSTR)strString.GetBuffer(100), 100);
+    double dValue = wcstod(strString, NULL);
+    strString.ReleaseBuffer();
+
+    return dValue;
 }
--- src/EyeTrackerDlg.cpp
+++ src/EyeTrackerDlg.cpp
@@ -15,17 +15,17 @@
 class CAboutDlg : public CDialog
 {
 public:
-	CAboutDlg();
+    CAboutDlg();
 
-// Dialog Data
-	enum { IDD = IDD_ABOUTBOX };
+    // Dialog Data
+    enum { IDD = IDD_ABOUTBOX };
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 // Implementation
 protected:
-	DECLARE_MESSAGE_MAP()
+    DECLARE_MESSAGE_MAP()
 };
 
 CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
@@ -34,7 +34,7 @@ CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
 
 void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
+    CDialog::DoDataExchange(pDX);
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
@@ -47,26 +47,26 @@ CEyeTrackerDlg* EyeTrackerDlg;
 
 
 CEyeTrackerDlg::CEyeTrackerDlg(CWnd* pParent /*=NULL*/)
-	: CDialog(CEyeTrackerDlg::IDD, pParent)
+    : CDialog(CEyeTrackerDlg::IDD, pParent)
 {
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
 void CEyeTrackerDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	DDX_Control(pDX, IDC_TAB, m_cTabCtrl);
+    CDialog::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_TAB, m_cTabCtrl);
 }
 
 BEGIN_MESSAGE_MAP(CEyeTrackerDlg, CDialog)
-	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
-	//}}AFX_MSG_MAP
+    ON_WM_SYSCOMMAND()
+    ON_WM_PAINT()
+    ON_WM_QUERYDRAGICON()
+    //}}AFX_MSG_MAP
 //	ON_NOTIFY(TCN_SELCHANGE, IDC_TAB, &CEyeTrackerDlg::OnTcnSelchangeTab)
 ON_COMMAND(ID_MENU_EXIT, &CEyeTrackerDlg::OnMenuExit)
 ON_COMMAND(ID_MENU_ABOUT, &CEyeTrackerDlg::OnMenuAbout)
-ON_MESSAGE(WM_NOTIFYICON,&CEyeTrackerDlg::OnTrayNotify)
+ON_MESSAGE(WM_NOTIFYICON, &CEyeTrackerDlg::OnTrayNotify)
 ON_WM_DESTROY()
 //ON_WM_KEYDOWN()
 END_MESSAGE_MAP()
@@ -76,86 +76,86 @@ END_MESSAGE_MAP()
 
 BOOL CEyeTrackerDlg::OnInitDialog()
 {
-	CDialog::OnInitDialog();
+    CDialog::OnInitDialog();
 
-	// Add "About..." menu item to system menu.
+    // Add "About..." menu item to system menu.
 
-	// IDM_ABOUTBOX must be in the system command range.
-	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
-	ASSERT(IDM_ABOUTBOX < 0xF000);
+    // IDM_ABOUTBOX must be in the system command range.
+    ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+    ASSERT(IDM_ABOUTBOX < 0xF000);
 
-	CMenu* pSysMenu = GetSystemMenu(FALSE);
-	if (pSysMenu != NULL)
-	{
-		CString strAboutMenu;
-		strAboutMenu.LoadString(IDS_ABOUTBOX);
-		if (!strAboutMenu.IsEmpty())
-		{
-			pSysMenu->AppendMenu(MF_SEPARATOR);
-			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
-		}
-	}
+    CMenu* pSysMenu = GetSystemMenu(FALSE);
+    if (pSysMenu != NULL)
+    {
+        CString strAboutMenu;
+        strAboutMenu.LoadString(IDS_ABOUTBOX);
+        if (!strAboutMenu.IsEmpty())
+        {
+            pSysMenu->AppendMenu(MF_SEPARATOR);
+            pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+        }
+    }
 
-	// Set the icon for this dialog.  The framework does this automatically
-	//  when the application's main window is not a dialog
-	SetIcon(m_hIcon, TRUE);			// Set big icon
-	SetIcon(m_hIcon, FALSE);		// Set small icon
+    // Set the icon for this dialog.  The framework does this automatically
+    //  when the application's main window is not a dialog
+    SetIcon(m_hIcon, TRUE);			// Set big icon
+    SetIcon(m_hIcon, FALSE);		// Set small icon
 
-	// TODO: Add extra initialization here
-	
-
-	m_cTabCtrl.InitDialogs();
-	TCITEM tcItem;
-	tcItem.mask = TCIF_TEXT;
-
-	tcItem.pszText = _T("Camera");
-	m_cTabCtrl.InsertItem(0,&tcItem);
-
-	tcItem.pszText = _T("Settings");
-	m_cTabCtrl.InsertItem(1,&tcItem);
-
-	tcItem.pszText = _T("Logs");
-	m_cTabCtrl.InsertItem(2,&tcItem);
-
-	tcItem.pszText = _T("Image");
-	m_cTabCtrl.InsertItem(3,&tcItem);
-
-	tcItem.pszText = _T("Comparer");
-	m_cTabCtrl.InsertItem(4,&tcItem);
-
-	m_cTabCtrl.ActivateTabDialogs();
+    // TODO: Add extra initialization here
 
 
-	CString sTip(_T("Eye Tracker")); 
-	m_cTnd.cbSize = sizeof(NOTIFYICONDATA);
-	m_cTnd.hWnd = this->GetSafeHwnd();;
-	m_cTnd.uID = IDR_MAINFRAME;
+    m_cTabCtrl.InitDialogs();
+    TCITEM tcItem;
+    tcItem.mask = TCIF_TEXT;
 
-	m_cTnd.uCallbackMessage = WM_NOTIFYICON;
-	m_cTnd.uFlags = NIF_MESSAGE|NIF_ICON|NIF_TIP; 
-	m_cTnd.hIcon = LoadIcon(AfxGetInstanceHandle(), MAKEINTRESOURCE (IDR_MAINFRAME));
+    tcItem.pszText = _T("Camera");
+    m_cTabCtrl.InsertItem(0, &tcItem);
 
-	lstrcpyn(m_cTnd.szTip, (LPCTSTR)sTip, sizeof(m_cTnd.szTip));
-	m_cTrayMenu.LoadMenu(IDR_MENU);
+    tcItem.pszText = _T("Settings");
+    m_cTabCtrl.InsertItem(1, &tcItem);
+
+    tcItem.pszText = _T("Logs");
+    m_cTabCtrl.InsertItem(2, &tcItem);
+
+    tcItem.pszText = _T("Image");
+    m_cTabCtrl.InsertItem(3, &tcItem);
+
+    tcItem.pszText = _T("Comparer");
+    m_cTabCtrl.InsertItem(4, &tcItem);
+
+    m_cTabCtrl.ActivateTabDialogs();
 
 
-	EyeTrackerDlg = this;
+    CString sTip(_T("Eye Tracker"));
+    m_cTnd.cbSize = sizeof(NOTIFYICONDATA);
+    m_cTnd.hWnd = this->GetSafeHwnd();;
+    m_cTnd.uID = IDR_MAINFRAME;
 
-	Log(L"Starting EyeTracker");
+    m_cTnd.uCallbackMessage = WM_NOTIFYICON;
+    m_cTnd.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+    m_cTnd.hIcon = LoadIcon(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_MAINFRAME));
 
-	return TRUE;  // return TRUE  unless you set the focus to a control
+    lstrcpyn(m_cTnd.szTip, (LPCTSTR)sTip, sizeof(m_cTnd.szTip));
+    m_cTrayMenu.LoadMenu(IDR_MENU);
+
+
+    EyeTrackerDlg = this;
+
+    Log(L"Starting EyeTracker");
+
+    return TRUE;  // return TRUE  unless you set the focus to a control
 }
 
 void CEyeTrackerDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
-	CDialog::OnSysCommand(nID, lParam);
+    CDialog::OnSysCommand(nID, lParam);
 
-	if(nID == SC_MINIMIZE)
-	{	
-		DWORD dwMessage = NIM_ADD;
-		ShowWindow(SW_HIDE);
-		Shell_NotifyIcon(dwMessage, &m_cTnd);    	
-	}
+    if (nID == SC_MINIMIZE)
+    {
+        DWORD dwMessage = NIM_ADD;
+        ShowWindow(SW_HIDE);
+        Shell_NotifyIcon(dwMessage, &m_cTnd);
+    }
 }
 
 // If you add a minimize button to your dialog, you will need the code below
@@ -164,34 +164,34 @@ void CEyeTrackerDlg::OnSysCommand(UINT nID, LPARAM lParam)
 
 void CEyeTrackerDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // device context for painting
+    if (IsIconic())
+    {
+        CPaintDC dc(this); // device context for painting
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+        SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// Center icon in client rectangle
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+        // Center icon in client rectangle
+        int cxIcon = GetSystemMetrics(SM_CXICON);
+        int cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// Draw the icon
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialog::OnPaint();
-	}
+        // Draw the icon
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CDialog::OnPaint();
+    }
 }
 
 // The system calls this function to obtain the cursor to display while the user drags
 //  the minimized window.
 HCURSOR CEyeTrackerDlg::OnQueryDragIcon()
 {
-	return static_cast<HCURSOR>(m_hIcon);
+    return static_cast<HCURSOR>(m_hIcon);
 }
 
 //void CEyeTrackerDlg::OnTcnSelchangeTab(NMHDR *pNMHDR, LRESULT *pResult)
@@ -202,54 +202,54 @@ HCURSOR CEyeTrackerDlg::OnQueryDragIcon()
 
 void CEyeTrackerDlg::Log(CString strMessage)
 {
-	SYSTEMTIME myTime;
-	
-	GetSystemTime(&myTime);
-		
-	CString strTime;
-	strTime.Format( L"%02d-%02d-%d %02d:%02d:%02d", myTime.wDay,myTime.wMonth, myTime.wYear, myTime.wHour, myTime.wMinute, myTime.wSecond );
-	
-	if(m_pLogDlg)
-		m_pLogDlg->InsertLog(strTime,strMessage);
+    SYSTEMTIME myTime;
+
+    GetSystemTime(&myTime);
+
+    CString strTime;
+    strTime.Format(L"%02d-%02d-%d %02d:%02d:%02d", myTime.wDay, myTime.wMonth, myTime.wYear, myTime.wHour, myTime.wMinute, myTime.wSecond);
+
+    if (m_pLogDlg)
+        m_pLogDlg->InsertLog(strTime, strMessage);
 }
 
 void CEyeTrackerDlg::OnMenuExit()
 {
-	SendMessage(WM_CLOSE);
+    SendMessage(WM_CLOSE);
 }
 
 void CEyeTrackerDlg::OnMenuAbout()
 {
-	CAboutDlg cAboutDlg;
-	//cAboutDlg.Create(IDD_ABOUTBOX,NULL);
-	cAboutDlg.DoModal();
+    CAboutDlg cAboutDlg;
+    //cAboutDlg.Create(IDD_ABOUTBOX,NULL);
+    cAboutDlg.DoModal();
 
 }
-LRESULT CEyeTrackerDlg::OnTrayNotify(WPARAM wParam,LPARAM lParam)
-  {
-    UINT uMsg = (UINT) lParam; 
-    switch (uMsg ) 
-    { 
-		case WM_LBUTTONDBLCLK:
-			this->ShowWindow(SW_SHOW);
-			SendMessage(WM_SYSCOMMAND, SC_RESTORE,0);
-			Shell_NotifyIcon(NIM_DELETE,&m_cTnd);
-			break;
-		case WM_RBUTTONUP:
-			CPoint pt;    
-			GetCursorPos(&pt);
-			SetForegroundWindow();
-			m_cTrayMenu.GetSubMenu(0)->TrackPopupMenu(TPM_RIGHTALIGN|TPM_LEFTBUTTON|TPM_RIGHTBUTTON,pt.x,pt.y,this);		
-			break;
-    } 
+LRESULT CEyeTrackerDlg::OnTrayNotify(WPARAM wParam, LPARAM lParam)
+{
+    UINT uMsg = (UINT)lParam;
+    switch (uMsg)
+    {
+    case WM_LBUTTONDBLCLK:
+        this->ShowWindow(SW_SHOW);
+        SendMessage(WM_SYSCOMMAND, SC_RESTORE, 0);
+        Shell_NotifyIcon(NIM_DELETE, &m_cTnd);
+        break;
+    case WM_RBUTTONUP:
+        CPoint pt;
+        GetCursorPos(&pt);
+        SetForegroundWindow();
+        m_cTrayMenu.GetSubMenu(0)->TrackPopupMenu(TPM_RIGHTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON, pt.x, pt.y, this);
+        break;
+    }
     return TRUE;
 }
 void CEyeTrackerDlg::OnDestroy()
 {
-	
-	
-	CDialog::OnDestroy();
 
-	// TODO: Add your message handler code here
+
+    CDialog::OnDestroy();
+
+    // TODO: Add your message handler code here
 }
 
