7aeeeff244cf3bb5ab7788f72bdd1e3ab5af4435
Przemyslaw Koziol <przemkoz85@gmail.com>
2020-05-16 22:58:30 +0200

    incorporate gtests

--- tests/test_objectDetection.cpp
+++ tests/test_objectDetection.cpp
@@ -2,11 +2,12 @@
 #include "opencv2/imgcodecs.hpp"
 #include <filesystem>
 #include <iostream>
+#include "gtest/gtest.h"
 
-
-int main() {
+TEST(objectDetection, testDetectFaces)
+{
     CObjectDetection::Init();
-    
+
     constexpr int expectedFacesDetected = 28;
     const std::string path = "bioid";
     std::error_code error;
@@ -15,9 +16,7 @@ int main() {
 
     if (error) {
         std::cout << error.message() << std::endl;
-        return error.value();
-    } 
-    else {     
+    } else {
         for (const auto& entry : std::filesystem::directory_iterator(path)) {
             CStringA filePath(CString { entry.path().c_str() });
             const auto img = cv::imread(static_cast<const char*>(filePath));
@@ -28,14 +27,12 @@ int main() {
                 }
             }
         }
-      
     }
+    EXPECT_EQ(detectedFaces, 28);
+}
 
-    if (detectedFaces != expectedFacesDetected) {
-        std::cout << "Detected faces: " << detectedFaces << std::endl;
-        std::cout << "Test failed" << std::endl;
-    } else {
-        std::cout << "Test passed" << std::endl;
-    }
-    return 0;
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
